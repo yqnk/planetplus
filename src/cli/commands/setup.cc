f7b9d76966c254dd9ad05da240d6ff76fb89b108
Yqnk <yan.bertile@gmail.com>
2024-02-16 19:09:37 -0500

    curl & mariadb added

--- src/cli/commands/setup.cc
+++ src/cli/commands/setup.cc
@@ -5,7 +5,38 @@
 #include <string>
 
 #include "cli/tools.h"
+#include "utils/ini_parser.h"
 
+namespace
+{
+/**
+ * @brief Check if user has mariadb installed
+ *
+ * @return int
+ */
+int check_for_mariadb()
+{
+#ifdef _WIN32
+    return system("mariadb --version");
+#else
+    return system("mariadb --version");
+#endif
+}
+
+int check_for_curl()
+{
+#ifdef _WIN32
+    return system("curl --version");
+#else
+    return system("curl --version");
+#endif
+}
+
+/**
+ * @brief Creates the default configuration file.
+ *
+ * @param filePath The path to the configuration file.
+ */
 void create_default_config_file(const std::string& filePath)
 {
     std::string configContent = R"(
@@ -24,8 +55,8 @@ password =
 [database]
 host = localhost ; or whatever your database host is
 port = 3306 ; or whatever your database port is
-user = mysql-user ; or whatever your database user is
-password = mysql-password ; or whatever your database password is
+user=mysql-user ; or whatever your database user is
+password=mysql-password ; or whatever your database password is
 name = planetplus
 
 [masteradmins]      ; the order doesn't matter
@@ -52,11 +83,15 @@ id[] = your-id-here ; id of the second admin
             cli_tools::bold(filePath));
     }
 }
+}
 
 namespace cli_commands
 {
 void planetplus_setup()
 {
+    cli_tools::clear_screen();
+    std::cout << "> Setting up PlanetPlus...\n" << std::endl;
+
     std::string base_dir_path = BASE_DIR;
     base_dir_path += "planetplus/";
 
@@ -76,6 +111,7 @@ void planetplus_setup()
             {
                 cli_tools::print_success(
                     "Removed directory: " + cli_tools::bold(base_dir_path));
+                putchar('\n');
             }
             else
             {
@@ -156,5 +192,70 @@ void planetplus_setup()
             "! Failed to create file: " + cli_tools::bold(chat_log_file));
         return;
     }
+
+    // Check for requirements
+
+    std::cout << "\n> Checking for requirements..." << std::endl;
+
+    // Check for MariaDB
+    std::cout << "\nChecking for MariaDB..." << std::endl;
+    if (check_for_mariadb() == 0)
+    {
+        cli_tools::print_success("MariaDB is installed !");
+    }
+    else
+    {
+        cli_tools::print_error(
+            "! MariaDB is not installed !\nPlease install MariaDB and try "
+            "again.\n");
+#ifdef _WIN32
+        std::cout << "https://www.mariadbtutorial.com/getting-started/"
+                     "install-mariadb/"
+                  << std::endl;
+#endif
+        return;
+    }
+
+    // Check for cURL
+    std::cout << "\nChecking for cURL..." << std::endl;
+    if (check_for_curl() == 0)
+    {
+        cli_tools::print_success("cURL is installed !");
+    }
+    else
+    {
+        cli_tools::print_error(
+            "! cURL is not installed !\nPlease install cURL and try again.\n");
+#ifdef _WIN32
+        std::cout << "cURL already installed ? Try adding it to your PATH\n"
+                     "https://curl.se/windows/"
+                  << std::endl;
+#endif
+    }
+
+    // Creating database folder
+    std::cout << "\n> Creating database...\n" << std::endl;
+
+    std::string database_path = base_dir_path + "database/";
+
+    if (cli_tools::create_dir(database_path.c_str()) == 0)
+    {
+        cli_tools::print_success(
+            "Created directory: " + cli_tools::bold(config_path));
+    }
+    else
+    {
+        cli_tools::print_error(
+            "! Failed to create directory: " + cli_tools::bold(config_path));
+        return;
+    }
+
+    std::string user = cli_tools::get_input("Enter your MariaDB username: ");
+    std::string password =
+        cli_tools::get_input("Enter your MariaDB password: ");
+
+    config::IniParser ini(config_path + "config.ini");
+    ini.set("database", "user", user);
+    ini.set("database", "password", password);
 }
-}
+} // namespace cli_commands
