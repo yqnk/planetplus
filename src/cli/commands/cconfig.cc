c410de8cdeffbf598648b01b073aec065fc2f621
Yqnk <yan.bertile@gmail.com>
2024-02-21 16:19:47 -0500

    typo

--- src/cli/commands/cconfig.cc
+++ src/cli/commands/cconfig.cc
@@ -28,7 +28,7 @@ int planetplusGetConfig(std::string section, std::string key)
     return CLI_EXIT_SUCCESS;
 }
 
-int PlanetplusSetConfig(
+int planetplusSetConfig(
     std::string section, std::string key, std::string value)
 {
     std::string base_dir_path = BASE_DIR;
